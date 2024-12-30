71bab5ed0f0b210ff83c216c90bd71142d2e970b
nefeltellioglu <nefel.tellioglu@gmail.com>
2024-12-30 12:59:31 +1100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 conda create -n GovComGPTQ  python=3.11.5 -y
+conda init zsh
+source ~/.zshrc
+/opt/anaconda3/bin/conda init
+source activate base
 conda activate GovComGPTQ
-conda install -c "nvidia/label/cuda-12.1.0" cuda-toolkit cuda -y
-conda install pytorch==2.2.1 torchvision==0.17.1 torchaudio==2.2.1 pytorch-cuda=12.1 -c pytorch -c nvidia
-
+conda install pytorch torchvision -c pytorch
+conda install pytorch::torchvision
+conda install pytorch torchvision torchaudio -c pytorch
 conda install conda-forge::weasyprint -y
 conda install -c conda-forge python-kaleido -y
 
 pip3 install -r pathfinder/requirements.txt
-pip3 install autogptq
+pip3 install auto_gptq
 pip3 install bitsandbytes
 
 pip3 install -r requirements.txt
 
 
-pip3 transformers==4.39.3
+conda install transformers==4.39.3
